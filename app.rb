708bbd8235656703aaee1e449d8cf4f95960e311
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-30 11:30:11 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Eduardo"
+  end
+
+  get "/hometown" do
+    "My hometown is Mexico"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is cielito lindo"
+  end
 end
